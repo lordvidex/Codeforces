4cf63adf871d6450490a00468ae032f9aee6b9f3
lordvidex <lordvidex.prime@gmail.com>
2021-02-18 17:00:36 +0300

    Saved space for class

--- src/com/unicorn/Main.java
+++ src/com/unicorn/Main.java
@@ -2,68 +2,53 @@ package com.unicorn;
 
 import java.util.*;
 import java.io.*;
-import java.util.stream.Collectors;
 
 public class Main {
     static FastScanner sc;
     static PrintWriter pw;
     static int cas = 1;
-    static String ans;
+    static long ans;
 
-    static HashMap<Character, char[]> keyPress = new HashMap<>() {{
-        put('2', new char[]{'A', 'B', 'C'});
-        put('3', new char[]{'D', 'E', 'F'});
-        put('4', new char[]{'G', 'H', 'I'});
-        put('5', new char[]{'J', 'K', 'L'});
-        put('6', new char[]{'M', 'N', 'O'});
-        put('7', new char[]{'P', 'Q', 'R', 'S'});
-        put('8', new char[]{'T', 'U', 'V'});
-        put('9', new char[]{'W', 'X', 'Y', 'Z'});
-    }};
-
-    static int[][] scoreBoard = new int[26][];
-    static TreeSet<Item> answerSet;
 
     public static void printOutput() {
         pw.println("Case #" + cas++ + ": " + ans);
     }
 
-    public static void bfs(String curr, LinkedList<Character> pressedNumbers, int score) {
-        if (pressedNumbers.size() == 0) {
-            answerSet.add(new Item(score, curr));
-            return;
-        }
-        char start = pressedNumbers.pop();
-        char[] startChars = keyPress.get(start);
-        char lastChar = curr.charAt(curr.length() - 1);
-        for (char startChar : startChars) {
-            bfs(curr + startChar, new LinkedList<>(pressedNumbers), score + scoreBoard[lastChar - 'A'][startChar - 'A']);
-        }
-    }
+    static long power(long x, long y, long p)
+    {
+        long res = 1; // Initialize result
 
+        x = x % p; // Update x if it is more than or
+        // equal to p
+
+        if (x == 0)
+            return 0; // In case x is divisible by p;
+
+        while (y > 0)
+        {
+
+            // If y is odd, multiply x with result
+            if ((y & 1) != 0)
+                res = (res * x) % p;
+
+            // y must be even now
+            y = y >> 1; // y = y/2
+            x = (x * x) % p;
+        }
+        return res;
+    }
     public static void solve() {
-        answerSet = new TreeSet<>();
-        int l = sc.ni();
-        int k = sc.ni();
-        for (int i = 0; i < 26; i++) {
-            scoreBoard[i] = new int[26];
-            for (int j = 0; j < 26; j++) {
-                scoreBoard[i][j] = sc.ni();
-            }
-        }
-        String userInput = sc.next();
-        LinkedList<Character> linkedList = userInput.chars().mapToObj(c -> (char) c).collect(Collectors.toCollection(LinkedList::new));
-        char start = linkedList.pop();
-        char[] startChars = keyPress.get(start);
-        for (char startChar : startChars) {
-            bfs(startChar + "", new LinkedList<>(linkedList), 0);
-        }
-        ans = answerSet.toArray(new Item[0])[k - 1].string;
+        long r = sc.nl();
+        long n = sc.nl();
+        long m = sc.nl();
+        n *= n;
+        ans = power(r,n,m)%m;
+
         printOutput();
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        sc = new FastScanner("input-3.txt");
+        sc = new FastScanner("input.txt");
         pw = new PrintWriter("output.txt");
         int t = sc.ni();
         while (t-- > 0) {
@@ -72,6 +57,29 @@ public class Main {
 
         sc.close();
         pw.close();
+    }
+
+    static class Point {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return x == point.x && y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
     }
 
     static class FastScanner {
@@ -135,33 +143,6 @@ public class Main {
                 e.printStackTrace();
             }
             return "";
-        }
-    }
-
-    static class Item implements Comparable<Item> {
-        int score;
-        String string;
-
-        public Item(int score, String string) {
-            this.score = score;
-            this.string = string;
-        }
-
-        @Override
-        public String toString() {
-            return "Item{" +
-                    "score=" + score +
-                    ", string='" + string + '\'' +
-                    '}';
-        }
-
-        @Override
-        public int compareTo(Item o) {
-            if (score != o.score) {
-                return o.score - score;
-            } else {
-                return string.compareTo(o.string);
-            }
         }
     }
 }
